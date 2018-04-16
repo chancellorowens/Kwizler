3bcf562870f8f2172e09a7a9b52572122169d1bb
student <sce60786@uga.edu>
2018-04-16 16:06:37 -0400

    Added in the test file for database connection and a DB Connection

--- test_src/kwizler/DBManagerTest.java
+++ test_src/kwizler/DBManagerTest.java
@@ -1,1 +1,50 @@
 //test file for DBManager
+
+package kwizler;
+import java.sql.*;
+import junit.framework.*;
+import java.util.ArrayList;
+
+public class DBManagerTest extends TestCase{
+
+
+    /* This function should test and make sure the connection is working                                                                                                                                                                                                                           
+     *properly.                                                                                                                                               */
+    public static void testConnection ()throws SQLException, ClassNotFoundException {
+	DBManager  myConnection= new DBManager();
+	assertEquals( "Established Connection",myConnection.getStatus());
+    }//testConnection
+
+
+    //This method tests the getter for the connection variable.
+    public static void testGetConn() throws SQLException, ClassNotFoundException{
+	DBManager conn= new DBManager();
+	if(conn.getConn()==null){
+	    assertEquals(false, true);
+	}else{
+	    assertEquals(true, true);
+	}
+
+
+
+    }//getConn
+
+
+    //This method tests the getter for the status variable.
+    public static void testGetStatus() throws SQLException, ClassNotFoundException{
+	DBManager conn= new DBManager();
+	if(conn.getStatus()==null){
+	    assertEquals(false, true);
+	}else{
+	    assertEquals(true, true);
+	}
+    }//testGetStatus
+
+
+
+
+
+
+}
+
+
