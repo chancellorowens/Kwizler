9290c2d695dc12d43fe4e58c61d42f53f494cb21
Chance <codarklight96@gmail.com>
2018-04-19 16:28:27 -0400

    Set up the build file and fixed the Kwizler java file

--- src/kwizler/Kwizler.java
+++ src/kwizler/Kwizler.java
@@ -8,34 +8,39 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Kwizler {
-    DBMangager db;
-    public static void main(String[] args){
-	DBManager db = new DBManager();
+    
+    DBManager db;
+    String[][] vocabList = new String[2][25]; 
+
+    public Kwizler() throws ClassNotFoundException, SQLException{
+	db = new DBManager();
     }
     /*
       Contains all the functioallity for adding a new sub-category for a subject
     */
-    public void addVocabList(){
-
+    public void setVocabList(String tableName, String[][] vocab) throws SQLException{
+	String query = "CREAT TABLE " + tableName + " (Word varchar(255), definition varchar(255));";
     }
     /*
       Creates a new table filled with vocab words and definitions for a anew sub-category
     */
-    public void createVocabListTable(){
+    public void createVocabListTable() throws SQLException{
 	
     }
     /*
       Gets all the entries in a sub-category table ready to be viewed in the jsp page
     */
-    public String[][] viewVocabList(String vocabListName){
-	PreparedStatement showStatement = conn.prepareStatement("SELECT * FROM ?");
-	searchStatement.setString(1,vocabListName);
-	rs = searchStatement.executeQuery();
+    public String[][] getVocabList(String vocabListName) throws SQLException{
+	PreparedStatement showStatement = db.conn.prepareStatement("SELECT * FROM ?");
+	showStatement.setString(1,vocabListName);
+	ResultSet rs = showStatement.executeQuery();
 	String [][] resultString = new String[2][25];
 	for(int i = 0; rs != null && rs.next(); i++){
 	    resultString[0][i] = rs.getString(1);//should be the vocab word 
 	    resultString[1][i] = rs.getString(2);
-	}	
+	}
+	vocabList = resultString;
+	return vocabList;
     }
 	
 }
